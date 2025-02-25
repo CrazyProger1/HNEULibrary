4cc052710765d59e7a07b98a7589ec5fe595732c
CrazyProger1 <crazyproger1@gmail.com>
2025-02-25 19:19:30 +0200

    initial commit

---
+++ backend/shop/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from shop import views as shop_views
+from . import views
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('shop/', shop_views.shop, name='shop'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('product/<int:product_id>/edit/', views.edit_product, name='edit_product'),
+    path('product/<int:product_id>/delete/', views.delete_product, name='delete_product'),
+]
