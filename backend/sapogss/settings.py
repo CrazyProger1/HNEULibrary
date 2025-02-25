de38ca12e736abbb560431b4079e7e676efd30bd
CrazyProger1 <crazyproger1@gmail.com>
2025-02-25 19:32:45 +0200

    add: poetry

--- backend/sapogss/settings.py
+++ backend/sapogss/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u0oay)u9m2rcq7uhynce-3sm9k!wm=uuqldgjou*k_&ida=8xv'
+SECRET_KEY = "django-insecure-u0oay)u9m2rcq7uhynce-3sm9k!wm=uuqldgjou*k_&ida=8xv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,54 +32,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'shop',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "shop",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'sapogss.urls'
+ROOT_URLCONF = "sapogss.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'sapogss.wsgi.application'
+WSGI_APPLICATION = "sapogss.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -89,16 +89,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -106,15 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 LANGUAGES = [
-    ('en', 'English'),
-    ('uk', 'Ukrainian'),
-    ('de', 'German'),
+    ("en", "English"),
+    ("uk", "Ukrainian"),
+    ("de", "German"),
 ]
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -126,7 +126,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -134,4 +134,4 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -4,71 +4,78 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import user_passes_test
 
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def shop(request):
-    query = request.GET.get('query')
+    query = request.GET.get("query")
     if query:
         products = Product.objects.filter(title__icontains=query)
     else:
         products = Product.objects.all()
-    return render(request, 'shop/shop.html', {'products': products})
+    return render(request, "shop/shop.html", {"products": products})
+
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'shop/product_detail.html', {'product': product})
+    return render(request, "shop/product_detail.html", {"product": product})
+
 
 def register_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')  # Перенаправление
+            return redirect("login")  # Перенаправление
     else:
         form = UserCreationForm()
-    return render(request, 'reg.html', {'form': form})
+    return render(request, "reg.html", {"form": form})
+
 
 def login_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect("home")
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, "login.html", {"form": form})
 
-#@user_passes_test(lambda u: u.is_staff or u.is_superuser)
+
+# @user_passes_test(lambda u: u.is_staff or u.is_superuser)
 def edit_product(request, product_id):
     # Получить товар по его ID
     product = Product.objects.get(pk=product_id)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         # Обработать форму редактирования товара
-        product.title = request.POST['title']
-        product.price = request.POST['price']
-        product.description = request.POST['description']
+        product.title = request.POST["title"]
+        product.price = request.POST["price"]
+        product.description = request.POST["description"]
         product.save()
         # Перенаправить пользователя на страницу с подробностями о товаре после редактирования
-        return redirect('product_detail', product_id=product.id)
-    
-    return render(request, 'shop/edit_product.html', {'product': product})
+        return redirect("product_detail", product_id=product.id)
 
-#@user_passes_test(lambda u: u.is_staff or u.is_superuser)
+    return render(request, "shop/edit_product.html", {"product": product})
+
+
+# @user_passes_test(lambda u: u.is_staff or u.is_superuser)
 def delete_product(request, product_id):
     # Получить товар по его ID
     product = Product.objects.get(pk=product_id)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         # Удалить товар
         product.delete()
         # Перенаправить пользователя на страницу со списком товаров после удаления
-        return redirect('shop')
-    
+        return redirect("shop")
+
     # Вернуть шаблон для подтверждения удаления товара
-    return render(request, 'shop/delete_product.html', {'product': product})
+    return render(request, "shop/delete_product.html", {"product": product})
