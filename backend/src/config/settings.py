f4ec511a353e5a57d1583aa50972252e4af43a25
CrazyProger1 <crazyproger1@gmail.com>
2025-02-27 12:22:02 +0200

    add: localization middleware

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "src.apps.accounts.middlewares.I18NMiddleware",
 ]
 
 ROOT_URLCONF = "src.config.urls"
