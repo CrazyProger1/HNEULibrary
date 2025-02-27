c3f1a8b5a707c63dc26ffcf2c8505f5cc6a46ea9
CrazyProger1 <crazyproger1@gmail.com>
2025-02-27 12:19:16 +0200

    update: book serializer

--- backend/src/apps/library/serializers/books.py
+++ backend/src/apps/library/serializers/books.py
@@ -11,4 +11,13 @@ class BookListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = (
+            "id",
+            "title",
+            "image",
+            "published_at",
+            "created_at",
+            "updated_at",
+            "author",
+            "genre",
+        )
--- backend/src/apps/library/urls.py
+++ backend/src/apps/library/urls.py
@@ -8,7 +8,7 @@ from src.apps.library.views import (
 
 router = routers.DefaultRouter()
 
-router.register("api/v1/library", BookViewSet)
+router.register("api/v1/books", BookViewSet)
 router.register("api/v1/genres", GenreViewSet)
 router.register("api/v1/authors", AuthorViewSet)
 
