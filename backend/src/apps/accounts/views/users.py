bfa146e152a3c4349091688175f05dae261fe129
CrazyProger1 <crazyproger1@gmail.com>
2025-03-06 21:39:07 +0200

    add: more logs

--- backend/src/apps/accounts/views/users.py
+++ backend/src/apps/accounts/views/users.py
@@ -28,8 +28,11 @@ class UserViewSet(
     def create(self, request, *args, **kwargs):
         logger.error("Data %s", request.data)
         serializer = self.get_serializer(data=request.data)
+        logger.error("SERIALIZER %s %s ", self.action, serializer)
         serializer.is_valid(raise_exception=True)
         data = serializer.data
+
+        logger.warning("DATA %s", data)
 
         create_user(
             username=data["username"],
