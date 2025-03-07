4e9a549e710acbea045c5be26b8c1a794068581e
CrazyProger1 <crazyproger1@gmail.com>
2025-03-08 00:02:37 +0200

    update: database to postgres

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -87,8 +87,12 @@ ASGI_APPLICATION = "src.config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
     }
 }
 
